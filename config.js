c8bc6ddfc543b24f072f1c12b3fdf3165bf0ee7d
tosahilgurung <70852292+tosahilgurung@users.noreply.github.com>
2020-09-06 23:39:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
+        
         "TO MY LOVE",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "MY BABY",  // 同上...
         "Today is your birthday",
-        "This is your first birthday after we together",
+        "This is your first birthday after we together",:
         "Even though I'm not with you",
         "Please don't get sad",
         "This distance will soon be a thing of the past",
@@ -25,7 +26,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "TO MY LOVE": "./imgs/Sahil1.JPG",
+        //"MY BABY",  // 同上...
+        //"Today is your birthday",
+        "This is your first birthday after we together": "./imgs/sahil3.jpg",
+        //"Even though I'm not with you",
+        //"Please don't get sad",
+        //"This distance will soon be a thing of the past",
+       // "and that can't come quickly enough",
+       // "I wish you all the health and happiness",
+        "A very Happy Birthday, My Love": "./imgs/Sahil Gurung.jpg",
+        "I LOVE YOU~~": "./imgs/Sahil2.JPG
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
